7ea7145e7cc22f8880b7fb112a39a1c564a83a93
Sumanaman <kumarsumannnnnn@gmail.com>
2023-01-21 12:51:32 -0500

    login page added

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -55,22 +55,131 @@ return loading ? (
   err ? (
     <h1>Refresh after 5 Seconds !</h1> ) :
     (
-      <>
-        {/* {data.products ?.data.products?.map((ele)=>
-        {
-           <div key={ele.id}>
-            <img src={ele.images} alt="" />
-           </div>
-        })} */}
-      </>
+      <div style={{
+        width:"95%",
+        height:"50rem",
+        margin:"auto",
+        marginTop:"50px",
+        marginBottom:"50px",
+        padding:"5px"
+        // border:"2px solid green",
+      }}>
+     <div style={{
+        width:"95%",
+        height:"auto",
+        margin:"auto",
+        display:"flex",
+      }}>
+        <div style={{
+          width:"35%",
+          position:"absolute"
+        }}>
+        <p style={{
+          width:"70%",
+            color:"#444",
+            fontSize:"1.5rem",
+            // border:"2px solid green",
+            textAlign:"left"
+        }}>REPUBLIC DAY OFFERS</p>
+        <p style={{
+            width:"70%",
+            color:"#444",
+            fontSize:"2rem",
+            textAlign:"left",
+            marginTop:"-15px",
+            marginBottom:"-10px"
+        }}>Be Free to Be More</p>
+        <p style={{
+          width:"100%",
+          height:"3rem",
+          textAlign:"left",
+          cursor:"pointer",
+        }}>Get 10% cashback up to <span style={{
+         color:"#0672cb"
+        }}>Rs</span> 5,000 on citi cards & 2 years onsite warranty at <span style={{
+          color:"#0672cb"
+         }}>Rs</span> 1* <div style={{
+          width:"1rem",
+          height:"1rem",
+          borderRadius:"50%",
+          border:"1px solid #0672cb",
+          textAlign:"center",
+          margin:"auto", 
+          marginTop:"-15px",
+          marginLeft:"125px",
+          cursor:"pointer"
+         }}>
+          <span style={{
+            color:"#0672cb",
+            fontWeight:"bold",
+          }}>i</span></div> </p>
+        <div style={{
+          width:"50%",
+          display:"flex",
+          justifyContent:"space-between"
+      }}>
+          <button style={{
+            padding:"10px",
+            fontSize:"1rem",
+            backgroundColor:"#0063c8",
+            border:"none",
+            color:"white",
+            cursor:"pointer"
+            // color:"#0063c8"
+          }}>Shop Deals</button>
+          <button style={{
+            padding:"10px",
+            fontSize:"1rem",
+            backgroundColor:"white",
+            border:"1px solid #0063c8",
+            cursor:"pointer",
+            color:"#0063c8"
+          }}> Monitor Deals</button>
+          </div>
+        </div>
+        <img style={{
+          width:"100%",
+          height:"100%",
+          marginTop:"-10px"
+      }} src="https://i.dell.com/is/image/DellContent/content/dam/ss2/page-specific/dell-homepage/apj/heroes/republic-day-cons-dell-xps-9315t-uhp-2304-12-in-homepage-hero-lf-2400x696-lr.png?fmt=pjpg&pscan=auto&scl=1&hei=696&wid=2400&qlt=100&resMode=sharp2&size=2400,696&op_usm=1.75,0.3,2,0" alt="Error" />
+      </div>
+
+      
+{/* sticky Contact Us */}
+
+      {/* <div style={{
+        width:"95%",
+        margin:"auto",
+        display:"flex",
+        flexDirection:"row-reverse",
+        position:"fixed"
+      }}>
+      <div style={{
+          width:"10rem",
+          height:"3rem",
+            backgroundColor:"#0063c8",
+            border:"none",
+            color:"white",
+            cursor:"pointer",
+            fontSize:"1.2rem",
+            display:"flex",
+            justifyContent:"space-around",
+            alignItems:"center",
+          }}>
+        <img style={{
+          width:"2rem",
+          height:"2rem",
+            borderRadius:"40%"
+        }} src="https://th.bing.com/th/id/OIP.75ZtLvJCC893G47VjE4zxAHaHa?w=215&h=215&c=7&r=0&o=5&pid=1.7" alt="Error"/>
+        <span >Contact Us</span>
+      </div>
+      </div> */}
+     <div style={{
+      border:"2px solid green"
+     }}>3 option   </div>
+      </div>
     )
 
 }
-
-// const Home = () => {
-
-//   return <h1>Home1</h1>;
-
-// }
 
 export default Home;
--- my-app/src/pages/SignIn.jsx
+++ my-app/src/pages/SignIn.jsx
@@ -2,20 +2,21 @@
 
 const SignIn = () => {
      return (
-        <div style={{width:"100%"}}>
+        <div style={{width:"100%",
+        display:"flex"}}>
             <div style={{
                     width:"50%"
                 }}>
                 <div style={{
                     // border:"2px solid green",
                     boxShadow: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
-                    width:"55%",
+                    width:"50%",
                     height:"1rem",
                     margin:"auto",
                     display:"flex",
                     justifyContent:"center",
                     alignItems:"center",
-                    padding:"10px 0px",
+                    padding:"15px 10px",
                     marginTop:"50px",
                     cursor:"pointer"
                 }}>
@@ -36,18 +37,20 @@ const SignIn = () => {
                     marginTop:"20px",
                     marginBottom:"20px"
                  }}>
-                 <input style={{width:"90%",padding:"5px",margin:"5px 0px"}} type="email" placeholder="Email Address"/>
-                 <input style={{width:"90%",padding:"5px",margin:"5px 0px"}} type="password" placeholder="Password"/>
+                 <input style={{width:"85%",padding:"10px 20px",margin:"10px 0px"}} type="email" placeholder="Email Address"/>
+                 <input style={{width:"85%",padding:"10px 20px",margin:"11px 0px"}} type="password" placeholder="Password"/>
                  <p style={{
-                    fontSize:"0.8rem"
+                    fontSize:"0.8rem",
+                    marginTop:"50px",
+                    cursor:"pointer"
                  }}>Don't remember your password? <span style={
                     {
                         color:"#0672cb"
                     }
                  }>Create or Reset password</span></p>
                  <input style={{
-                    width:"90%",
-                    padding:"10px",
+                   width:"95%",
+                   padding:"10px 20px",
                     margin:"20px 0px",
                     textAlign:"center",
                     cursor:"pointer",
@@ -55,7 +58,9 @@ const SignIn = () => {
                     border:"none",
                     color:"#FFFFFF"
                 }} type="submit" placeholder="Sign In"/>
-                <p style={{color:"gray"}}>or</p>
+                <p style={{color:"gray",
+            marginTop:"-5px",
+            marginBottom:"-5px",}}>or</p>
                 <input style={{
                     width:"90%",
                     padding:"10px",
@@ -73,7 +78,67 @@ const SignIn = () => {
                  }>Create an account</span></p>
                  </div>
             </div>
-             
+             <div style={{
+                width:"60%",
+                height:"auto",
+                margin:"auto",
+                backgroundColor:"#f5f6f7"
+             }}>
+               <p style={{
+                fontSize:"1.8rem",color:"#48494B"
+               }}>Dell Account Benifits</p>
+               <p style={{
+                fontSize:"0.8rem",color:"gray",
+                marginTop:"-10px"
+               }}>My Account is an easy,more centralized way to manage your tech.Sign Up Now!</p>
+               <div style={{
+                width:"100%",
+                height:"auto",
+                display:"flex",
+                justifyContent:"space-evenly",
+               }}>
+                <div style={{
+                padding:"5px",
+                width:"30%"
+               }}>
+                    <img src="https://i.dell.com/sites/csimages/App-Merchandizing_Images/all/icon-1-tracking-36x36.png" alt="Error" />
+                    <p>Order Tracking</p>
+                    <p style={{
+                        fontSize:"0.8rem",
+                        color:"gray"
+                    }}>Manage orders,track shipping status and view support history</p>
+                </div>
+                <div style={{
+                // border:"2px solid green",
+                width:"30%"
+                }}>
+                    <img src="https://i.dell.com/sites/csimages/App-Merchandizing_Images/all/icon-2-mail-36x36.png" alt="Error" />
+                    <p>Manage Communications</p>
+                    <p style={{
+                        fontSize:"0.8rem",
+                        color:"gray"
+                    }}>Manage marketing and communications preferences</p>
+                </div>
+                <div style={{
+                width:"25%",
+                textAlign:"center"}}>
+                    <img src="https://i.dell.com/sites/csimages/App-Merchandizing_Images/all/icon-3-profile-36x36.png" alt="Error" />
+                    <p>Profile Setting</p>
+                    <p style={{
+                        fontSize:"0.8rem",
+                        color:"gray"
+                    }}>Save your favorite payment method and shipping details for quicker checkout</p>
+                </div>
+               </div>
+               <div style={{
+                    width:"100%",
+                    // border:"2px solid green"
+                }}>
+                    <img style={{
+                        width:"50%"
+                    }} src="https://i.dell.com/sites/csimages/Banner_Imagery/all/bannerimage460x460.png" alt="Error" />
+                </div>
+             </div>
         </div>
      );
 }
